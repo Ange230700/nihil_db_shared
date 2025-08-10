e9b9c2b079b24077a526e0ca553f467f655309d7
Ange230700 <kouakouangeericstephane@gmail.com>
2025-08-10 17:38:37 +0200

    chore: align local changes

--- shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-// shared\src\index.ts
-
-export * from "./enums";
-export * from "./types";
-export * from "./utils";
--- shared/src/types.ts
+++
@@ -1,9 +0,0 @@
-// shared\types.ts
-
-import { UserRole } from "./enums";
-
-export interface JWTUserPayload {
-  id: string;
-  email: string;
-  role: UserRole;
-}
